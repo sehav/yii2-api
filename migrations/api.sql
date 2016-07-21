e574f33cd37efbb3a8eddfe82ee8e2d8b9bdcdbf
Sehav <sehav@bk.ru>
2016-07-21 13:25:04 +0300

    Last

--- migrations/api.sql
+++ migrations/api.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Июл 21 2016 г., 12:22
+-- Время создания: Июл 21 2016 г., 13:24
 -- Версия сервера: 5.5.48
 -- Версия PHP: 5.4.45
 
@@ -23,62 +23,21 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `authors`
---
-
-CREATE TABLE IF NOT EXISTS `authors` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
-
---
--- Дамп данных таблицы `authors`
---
-
-INSERT INTO `authors` (`id`, `name`) VALUES
-(19, 'Дж. К. Роулинг.');
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `authors_books`
---
-
-CREATE TABLE IF NOT EXISTS `authors_books` (
-  `id` int(10) unsigned NOT NULL,
-  `author_id` int(10) unsigned NOT NULL,
-  `book_id` int(10) unsigned NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
-
---
--- Дамп данных таблицы `authors_books`
---
-
-INSERT INTO `authors_books` (`id`, `author_id`, `book_id`) VALUES
-(3, 19, 6),
-(4, 19, 7),
-(5, 19, 8),
-(6, 19, 9),
-(7, 19, 10);
-
--- --------------------------------------------------------
-
---
 -- Структура таблицы `books`
 --
 
 CREATE TABLE IF NOT EXISTS `books` (
   `id` int(10) unsigned NOT NULL,
   `title` varchar(255) NOT NULL,
-  `cover` varchar(255) NOT NULL
+  `coverUrl` varchar(255) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `books`
 --
 
-INSERT INTO `books` (`id`, `title`, `cover`) VALUES
-(6, 'Гарри Поттер и философский камень', '1'),
+INSERT INTO `books` (`id`, `title`, `coverUrl`) VALUES
+(6, 'Гарри Поттер и философский камень2', '111'),
 (7, 'Гарри Поттер и Тайная комната', '2'),
 (8, 'Гарри Поттер и узник Азкабана', '3'),
 (9, 'Гарри Поттер и Кубок огня', '4'),
@@ -91,18 +50,6 @@ INSERT INTO `books` (`id`, `title`, `cover`) VALUES
 --
 
 --
--- Индексы таблицы `authors`
---
-ALTER TABLE `authors`
-  ADD PRIMARY KEY (`id`);
-
---
--- Индексы таблицы `authors_books`
---
-ALTER TABLE `authors_books`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Индексы таблицы `books`
 --
 ALTER TABLE `books`
@@ -112,16 +59,6 @@ ALTER TABLE `books`
 -- AUTO_INCREMENT для сохранённых таблиц
 --
 
---
--- AUTO_INCREMENT для таблицы `authors`
---
-ALTER TABLE `authors`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=20;
---
--- AUTO_INCREMENT для таблицы `authors_books`
---
-ALTER TABLE `authors_books`
-  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT для таблицы `books`
 --
